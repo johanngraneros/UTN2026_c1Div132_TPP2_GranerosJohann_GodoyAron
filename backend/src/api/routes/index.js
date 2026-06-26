1258f12773c2775b2936902f3e53ffe2c0670754
Johann Graneros <granerosjohann05@gmail.com>
2026-06-25 21:08:29 -0300

    centralizar rutas en archivo de barril

---
+++ backend/src/api/routes/index.js
@@ -0,0 +1,17 @@
+/*========================
+    Archivo de barril
+========================*/
+import productRoutes from "./product.routes.js";
+import viewRoutes from "./view.routes.js";
+
+// Eventualmente importaremos y unificaremos en este archivo "de barril" todas las rutas para centralizarlas y exportalas con un onmbre
+// import userRoutes from "./user.routes.js"
+// import authRoutes from "./auth.routes.js"
+
+// Archivo de barril que contiene todas las rutas
+export {
+    productRoutes,
+    viewRoutes
+    // userRoutes
+    // authRoutes
+}
