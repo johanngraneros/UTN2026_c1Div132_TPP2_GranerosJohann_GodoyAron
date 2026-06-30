ba59770f2112fb3311d6407965d9453d64e9f713
Johann Graneros <granerosjohann05@gmail.com>
2026-06-30 20:47:53 -0300

    Agrego clave de la session a variable de entorno

--- backend/src/api/config/environments.js
+++ backend/src/api/config/environments.js
@@ -6,10 +6,11 @@ dotenv.config(); // Cargamos las variables de entorno
 
 export default {
     port: process.env.PORT || 3001,
+    session_key: process.env.SESSION_KEY,
     database: {
         host: process.env.DB_HOST,
         name: process.env.DB_NAME,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD
     }
-}
+}   
