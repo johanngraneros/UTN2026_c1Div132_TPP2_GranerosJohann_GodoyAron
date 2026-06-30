c526efd0141ba9db3f7a8459c823490df63b3736
Johann Graneros <granerosjohann05@gmail.com>
2026-06-29 22:04:27 -0300

    agregar rutas de autenticacion al barril

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -3,15 +3,15 @@
 ========================*/
 import productRoutes from "./product.routes.js";
 import viewRoutes from "./view.routes.js";
+import authRoutes from "./auth.routes.js"
 
 // Eventualmente importaremos y unificaremos en este archivo "de barril" todas las rutas para centralizarlas y exportalas con un onmbre
 // import userRoutes from "./user.routes.js"
-// import authRoutes from "./auth.routes.js"
 
 // Archivo de barril que contiene todas las rutas
 export {
     productRoutes,
-    viewRoutes
+    viewRoutes,
+    authRoutes
     // userRoutes
-    // authRoutes
 }
