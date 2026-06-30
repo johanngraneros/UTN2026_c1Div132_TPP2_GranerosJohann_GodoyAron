db627e7b48b410ab6c1e540e2c182c6873a0221b
Johann Graneros <granerosjohann05@gmail.com>
2026-06-29 22:11:06 -0300

    Proteger vistas con login

--- backend/src/api/routes/view.routes.js
+++ backend/src/api/routes/view.routes.js
@@ -5,31 +5,35 @@
 import { Router } from "express";
 import { join, __dirname } from "../utils/index.js";
 import { createProductView, deleteProductView, getProductView, indexView, updateProductView } from "../controllers/view.controllers.js";
+import { requireLogin } from "../middlewares/middlewares.js";
 
 const router = Router();
 
+// Con el middleware requireLogin redirigimos a login para proteger la vista
+
 ////////////////////
 // Vista principal
-router.get("/index", indexView);
+router.get("/index", requireLogin, indexView); 
 
 
 ////////////////////
 // Vista obtener producto
-router.get("/consultar", getProductView);
+router.get("/consultar", requireLogin, getProductView);
 
 
 ////////////////////
 // Vista crear producto
-router.get("/crear", createProductView);
+router.get("/crear", requireLogin, createProductView);
 
 
 ////////////////////
 // Vista modificar producto
-router.get("/modificar", updateProductView);
+router.get("/modificar", requireLogin, updateProductView);
 
 
 ////////////////////
 // Vista eliminar producto
-router.get("/eliminar", deleteProductView);
+router.get("/eliminar", requireLogin, deleteProductView);
+
 
 export default router;
