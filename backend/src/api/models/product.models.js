d07e50150d0622f1e7aa2d90ff8bc8fef7b0291c
Johann Graneros <granerosjohann05@gmail.com>
2026-06-30 20:48:26 -0300

    Conecto los nombres de la consulta con la base de datos

--- backend/src/api/models/product.models.js
+++ backend/src/api/models/product.models.js
@@ -8,8 +8,8 @@ import connection from "../database/db.js";
 /////////////////////////////////
 // Traer todos los productos
 const selectAllProducts = () => {
-    // Optimizacion 3: Sacamos * para evitar traer columnas innecesarias -> Mas eficiente en memoria y peticion de red. Ademas separamos la sentencia en una variable
-    const sql = "SELECT id, nombre, precio, imagen FROM productos";
+  
+    const sql = "SELECT id, nombre, precio, imagen, categoria FROM productos";
     return connection.query(sql);
 }
 
@@ -18,8 +18,7 @@ const selectAllProducts = () => {
 /////////////////////////////////
 // Traer producto por id
 const selectProductById = (id) => {
-    // Optimizacion 4: Guardamos la consulta sql en una variable y la optimizamos pidiendo solo los campos requereidos
-    const sql = "SELECT id, nombre, precio, imagen FROM productos where products.id = ?";
+    const sql = "SELECT id, nombre, precio, imagen, categoria FROM productos where productos.id = ?";
     return connection.query(sql, [id]);
 }
 
@@ -28,9 +27,8 @@ const selectProductById = (id) => {
 /////////////////////////////////
 // Crear producto
 const insertNewProduct = (nombre, imagen, categoria, precio  ) => {
-    const sql = "INSERT INTO products (nombre, imagen, categoria, precio ) VALUES (?, ?, ?, ?)";
+    const sql = "INSERT INTO productos (nombre, imagen, categoria, precio ) VALUES (?, ?, ?, ?)";
     
-    // Optimizacion 3: Devolvemos la respuesta en un rows para devolver info util como el id asignado al nuevo producto
     return connection.query(sql, [nombre, imagen, categoria, precio ]);
 }
 
@@ -39,7 +37,7 @@ const insertNewProduct = (nombre, imagen, categoria, precio  ) => {
 /////////////////////////////////
 // Modificar producto
 const updateProduct = (nombre, imagen, precio, categoria, id) => {
-    const sql = "UPDATE products SET name = ?, imagen = ?, precio = ?, categoria = ? WHERE id = ?";
+    const sql = "UPDATE productos SET nombre = ?, imagen = ?, precio = ?, categoria = ? WHERE id = ?";
     
     // Guardamos el resultado de la conexion que nos bridara info para la optimziacion
     return connection.query(sql, [nombre, imagen, precio, categoria, id]);
