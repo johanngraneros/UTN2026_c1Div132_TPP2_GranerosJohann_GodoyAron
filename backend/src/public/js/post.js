e1972256f93b111263eed892a0b34d47f8a8268d
Johann Graneros <granerosjohann05@gmail.com>
2026-07-03 01:18:48 -0300

    Limpio comentarios

--- backend/src/public/js/post.js
+++ backend/src/public/js/post.js
@@ -24,7 +24,6 @@ function validarFormulario(data) {
     return errores;
 }
 
-
 // Optimizacion 2: Mensaje de exito o error al crear el producto
 // TO DO, le pasamos el errores.length
 function mostrarMensaje(tipo, mensaje) {
@@ -33,51 +32,6 @@ function mostrarMensaje(tipo, mensaje) {
     `;
 }
 
-//////////////////////
-// Enviando usuario
-// postUserForm.addEventListener("submit", async event => {
-//     event.preventDefault(); // Evitamos el envio por defecto del formulario
-
-//     // Obtenemos la data del formulario
-//     const formData = new FormData(event.target);
-
-//     // Convertimos nuestro objeto formdata en un objeto literal de JavaScript
-//     const data = Object.fromEntries(formData.entries());
-//     console.table(data);
-
-//     const jsonData = JSON.stringify(data);
-//     console.log(jsonData);
-
-//     try {
-        
-//         const response = await fetch("http://localhost:3000/api/users/", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: jsonData
-//         });
-
-//         console.log(response);
-//         const result = await response.json();
-
-//         if (!response.ok) {
-//             mostrarMensaje("error", result.message);
-//             return;
-//         }
-
-//         // Mostramos el mensaje de exito y reseteamos el form
-//         const infoUser = `${result.message} con id ${result.userId}`
-//         mostrarMensaje("exito", infoUser)
-//         console.log(infoUser);
-
-//         event.target.reset();
-
-//     } catch (error) {
-//         console.error("Error al enviar los datos: ", error);
-//     }
-
-// });
 
 //////////////////////
 // Enviando producto
