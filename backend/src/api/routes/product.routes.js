96bbc650bd66cec2aa641a445be19ccf2fa8ea30
Johann Graneros <granerosjohann05@gmail.com>
2026-07-03 00:19:50 -0300

    Agrego mayor proteccion a las rutas

--- backend/src/api/routes/product.routes.js
+++ backend/src/api/routes/product.routes.js
@@ -3,30 +3,30 @@
 ========================*/
 
 import { Router } from "express";
-import { middlewareBostero, validateId, validateProduct } from "../middlewares/middlewares.js";
+import { middlewareBostero, validateId, validateProduct, requireLoginApi} from "../middlewares/middlewares.js";
 import { createProduct, getAllProducts, getProductById, modifyProduct, removeProduct } from "../controllers/product.controllers.js";
 
 const router = Router();
 
 
 // GET all products
-router.get("/", getAllProducts);
+router.get("/", getAllProducts); //cliente necesita ver los productos, entonces no lo protegemos con requireLoginApi
 
 
 // GET by id
-router.get("/:id", validateId, getProductById);
+router.get("/:id", validateId, getProductById); //cliente necesita ver los productos, entonces no lo protegemos con requireLoginApi
 
 
 // POST product
-router.post("/", middlewareBostero ,validateProduct, createProduct);
+router.post("/", requireLoginApi, middlewareBostero ,validateProduct, createProduct);
 
 
 // UPDATE product
-router.put("/", modifyProduct);
+router.put("/", requireLoginApi, modifyProduct);
 
 
 // DELETE product
-router.delete("/:id", validateId, removeProduct);
+router.delete("/:id", requireLoginApi, validateId, removeProduct);   
 
 
 // Exportamos todas las rutas y las centralizamos en el archivo de barril -> index.js
--- backend/src/api/routes/user.routes.js
+++ backend/src/api/routes/user.routes.js
@@ -4,10 +4,11 @@
 
 import { Router } from "express";
 import { createAdminUser } from "../controllers/user.controllers.js";
+import { requireLoginApi } from "../middlewares/middlewares.js";
 const router = Router();
 
 // POST product
-router.post("/", createAdminUser);
+router.post("/", requireLoginApi, createAdminUser); 
 
 
 // Exportamos todas las rutas y las centralizamos en el archivo de barril -> index.js
