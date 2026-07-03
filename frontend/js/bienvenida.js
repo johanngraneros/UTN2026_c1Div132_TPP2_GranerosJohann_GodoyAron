ef25982db42e9a23b8918d92ff85c6ab03cf2856
Johann Graneros <granerosjohann05@gmail.com>
2026-07-03 00:55:53 -0300

    Valida nombre del cliente en bienvenida

--- frontend/js/bienvenida.js
+++ frontend/js/bienvenida.js
@@ -12,6 +12,27 @@ formBienvenida.addEventListener("submit", (event) =>
         return;
     }
 
+    let nombreValido = true;
+
+    for (let i = 0; i < nombreUsuario.length; i++) 
+    {
+        const caracter = nombreUsuario[i].toLowerCase();
+
+        const esLetra = caracter >= "a" && caracter <= "z";
+        const esEspacio = caracter === " ";
+
+        if (!esLetra && !esEspacio) 
+        {
+            nombreValido = false;
+        }
+    }
+
+    if (!nombreValido) 
+    {
+        alert("El nombre solo puede contener letras y espacios");
+        return;
+    }
+
     sessionStorage.setItem("nombreUsuario", nombreUsuario);
 
     window.location.href = "./productos.html";
